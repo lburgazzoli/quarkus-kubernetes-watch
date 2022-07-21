e867c9c2f195595336b9a87fa46db93a635f720f
Luca Burgazzoli <lburgazzoli@gmail.com>
2022-07-21 16:46:12 +0200

    cleanup

--- src/main/java/io/github/lburgazzoli/qw/MyWatcher.java
+++ src/main/java/io/github/lburgazzoli/qw/MyWatcher.java
@@ -15,16 +15,20 @@ import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.Watch;
 import io.fabric8.kubernetes.client.Watcher;
 import io.fabric8.kubernetes.client.WatcherException;
+import io.fabric8.kubernetes.client.informers.ResourceEventHandler;
+import io.fabric8.kubernetes.client.informers.SharedIndexInformer;
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
 
 @ApplicationScoped
-public class MyWatcher implements Watcher<GenericKubernetesResource> {
+public class MyWatcher {
     @Inject
     Logger log; 
     @Inject
     KubernetesClient client;
 
+    @ConfigProperty(name = "watch.mode", defaultValue = "watch")
+    String mode;
     @ConfigProperty(name = "watch.apiVersion")
     String apiVersion;
     @ConfigProperty(name = "watch.kind")
@@ -35,42 +39,102 @@ public class MyWatcher implements Watcher<GenericKubernetesResource> {
     Optional<Long> timeout;
 
     private Watch w;
+    private SharedIndexInformer<GenericKubernetesResource> i;
         
-    @Override
-    public void eventReceived(Action action, GenericKubernetesResource resource) {
-        log.infof("Action: %s, Namespace: %s, Name: %s", 
-            action.name(), 
-            resource.getMetadata().getNamespace(), 
-            resource.getMetadata().getName());                    
-    }
+    void onStart(@Observes StartupEvent ev) {   
+        if (mode.equals("watch") || mode.equals("all")) {    
 
-    @Override
-    public void onClose(WatcherException cause) {
-        log.warnf("Closed: %s", cause.getMessage());                 
-    }  
+            log.info("The watcher is starting...");
+            
+            namespace.ifPresentOrElse(
+                ns -> {
+                    w = client.genericKubernetesResources(apiVersion, kind).inNamespace(ns).watch(
+                        timeout.isPresent() 
+                            ? new ListOptionsBuilder().withTimeoutSeconds(timeout.get()).build() 
+                            : new ListOptionsBuilder().build(),
+                        new ResourceWatcher());
+                }, 
+                () -> {
+                    w = client.genericKubernetesResources(apiVersion, kind).inAnyNamespace().watch(
+                        timeout.isPresent() 
+                            ? new ListOptionsBuilder().withTimeoutSeconds(timeout.get()).build() 
+                            : new ListOptionsBuilder().build(),
+                        new ResourceWatcher());
+                });
+        }
 
-    void onStart(@Observes StartupEvent ev) {               
-        log.info("The application is starting...");
+        if (mode.equals("inform") || mode.equals("all")) {   
+            log.info("The informer is starting...");
 
-        
-        namespace.ifPresentOrElse(
-            ns -> {
-                w = client.genericKubernetesResources(apiVersion, kind).inNamespace(ns).watch(
-                    timeout.isPresent() ? new ListOptionsBuilder().withTimeoutSeconds(timeout.get()).build() : new ListOptionsBuilder().build(),
-                    this);
-            }, 
-            () -> {
-                w = client.genericKubernetesResources(apiVersion, kind).inAnyNamespace().watch(
-                    timeout.isPresent() ? new ListOptionsBuilder().withTimeoutSeconds(timeout.get()).build() : new ListOptionsBuilder().build(),
-                    this);
-            });
+            namespace.ifPresentOrElse(
+                ns -> {
+                    i = client.genericKubernetesResources(apiVersion, kind).inNamespace(ns).inform(new ResourceHandler());
+                }, 
+                () -> {
+                    i = client.genericKubernetesResources(apiVersion, kind).inAnyNamespace().inform(new ResourceHandler());
+                }); 
+        }
     }
 
     void onStop(@Observes ShutdownEvent ev) {               
         log.info("The application is stopping...");
 
-        if (w != null) {
-            w.close();
+        try {
+            if (w != null) {
+                w.close();
+            }
+        } catch (Exception e) {
+            // ignored            
         }
+
+        try {
+            if (i != null) {
+                i.close();
+            }
+        } catch (Exception e) {
+            // ignored            
+        }
+    }
+
+    private class ResourceWatcher implements Watcher<GenericKubernetesResource> {
+
+        @Override
+        public void eventReceived(Action action, GenericKubernetesResource resource) {
+            log.infof("Action: %s, Namespace: %s, Name: %s", 
+                action.name(), 
+                resource.getMetadata().getNamespace(), 
+                resource.getMetadata().getName());                    
+        }
+
+        @Override
+        public void onClose(WatcherException cause) {
+            log.warnf("Closed: %s", cause.getMessage());                 
+        }  
+    }
+
+    private class ResourceHandler implements ResourceEventHandler<GenericKubernetesResource> {
+        @Override
+        public void onAdd(GenericKubernetesResource resource) {
+            log.infof("Add, Namespace: %s, Name: %s", 
+                resource.getMetadata().getNamespace(), 
+                resource.getMetadata().getName()); 
+            
+        }
+
+        @Override
+        public void onUpdate(GenericKubernetesResource oldObj, GenericKubernetesResource resource) {
+            log.infof("Update, Namespace: %s, Name: %s",
+                resource.getMetadata().getNamespace(), 
+                resource.getMetadata().getName()); 
+            
+        }
+
+        @Override
+        public void onDelete(GenericKubernetesResource resource, boolean deletedFinalStateUnknown) {
+            log.infof("Delete, Namespace: %s, Name: %s",
+                resource.getMetadata().getNamespace(), 
+                resource.getMetadata().getName()); 
+            
+        }  
     }
 }

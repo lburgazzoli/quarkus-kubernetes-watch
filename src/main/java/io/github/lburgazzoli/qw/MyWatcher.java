f03a1b206ab19eaef2f1a9f91f76a46bedc468b3
Luca Burgazzoli <lburgazzoli@gmail.com>
2022-08-02 15:33:13 +0200

    add reconnect

--- src/main/java/io/github/lburgazzoli/qw/MyWatcher.java
+++ src/main/java/io/github/lburgazzoli/qw/MyWatcher.java
@@ -1,6 +1,9 @@
 package io.github.lburgazzoli.qw;
 
 import java.util.Optional;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
@@ -12,6 +15,7 @@ import org.jboss.logging.Logger;
 import io.fabric8.kubernetes.api.model.GenericKubernetesResource;
 import io.fabric8.kubernetes.api.model.ListOptionsBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientException;
 import io.fabric8.kubernetes.client.Watch;
 import io.fabric8.kubernetes.client.Watcher;
 import io.fabric8.kubernetes.client.WatcherException;
@@ -23,7 +27,7 @@ import io.quarkus.runtime.StartupEvent;
 @ApplicationScoped
 public class MyWatcher {
     @Inject
-    Logger log; 
+    Logger log;
     @Inject
     KubernetesClient client;
 
@@ -38,103 +42,158 @@ public class MyWatcher {
     @ConfigProperty(name = "watch.timeout")
     Optional<Long> timeout;
 
-    private Watch w;
-    private SharedIndexInformer<GenericKubernetesResource> i;
-        
-    void onStart(@Observes StartupEvent ev) {   
-        if (mode.equals("watch") || mode.equals("all")) {    
+    private ResourceWatcher w;
+    private ResourceHandler i;
+    private ScheduledExecutorService executor;
 
-            log.info("The watcher is starting...");
-            
-            namespace.ifPresentOrElse(
-                ns -> {
-                    w = client.genericKubernetesResources(apiVersion, kind).inNamespace(ns).watch(
-                        timeout.isPresent() 
-                            ? new ListOptionsBuilder().withTimeoutSeconds(timeout.get()).build() 
-                            : new ListOptionsBuilder().build(),
-                        new ResourceWatcher());
-                }, 
-                () -> {
-                    w = client.genericKubernetesResources(apiVersion, kind).inAnyNamespace().watch(
-                        timeout.isPresent() 
-                            ? new ListOptionsBuilder().withTimeoutSeconds(timeout.get()).build() 
-                            : new ListOptionsBuilder().build(),
-                        new ResourceWatcher());
-                });
+    void onStart(@Observes StartupEvent ev) {
+        executor = Executors.newSingleThreadScheduledExecutor();
+
+        if (mode.equals("watch") || mode.equals("all")) {
+            w = new ResourceWatcher();
+            w.start();
         }
 
-        if (mode.equals("inform") || mode.equals("all")) {   
-            log.info("The informer is starting...");
-
-            namespace.ifPresentOrElse(
-                ns -> {
-                    i = client.genericKubernetesResources(apiVersion, kind).inNamespace(ns).inform(new ResourceHandler());
-                }, 
-                () -> {
-                    i = client.genericKubernetesResources(apiVersion, kind).inAnyNamespace().inform(new ResourceHandler());
-                }); 
+        if (mode.equals("inform") || mode.equals("all")) {
+            i = new ResourceHandler();
+            i.start();
         }
     }
 
-    void onStop(@Observes ShutdownEvent ev) {               
+    void onStop(@Observes ShutdownEvent ev) {
         log.info("The application is stopping...");
 
-        try {
-            if (w != null) {
-                w.close();
-            }
-        } catch (Exception e) {
-            // ignored            
+        if (w != null) {
+            w.close();
         }
 
-        try {
-            if (i != null) {
-                i.close();
-            }
-        } catch (Exception e) {
-            // ignored            
+        if (i != null) {
+            i.close();
+        }
+
+        if (executor != null) {
+            executor.shutdownNow();
         }
     }
 
-    private class ResourceWatcher implements Watcher<GenericKubernetesResource> {
+    private class ResourceWatcher implements Watcher<GenericKubernetesResource>, AutoCloseable {
+        private Watch w;
 
         @Override
         public void eventReceived(Action action, GenericKubernetesResource resource) {
-            log.infof("Action: %s, Namespace: %s, Name: %s", 
-                action.name(), 
-                resource.getMetadata().getNamespace(), 
-                resource.getMetadata().getName());                    
+            log.infof("Action: %s, Namespace: %s, Name: %s",
+                action.name(),
+                resource.getMetadata().getNamespace(),
+                resource.getMetadata().getName());
         }
 
         @Override
         public void onClose(WatcherException cause) {
-            log.warnf("Closed: %s", cause.getMessage());                 
-        }  
+            if (cause == null) {
+                return;
+            }
+
+            log.warnf("Closed: %s", cause.getMessage());
+
+            if (cause.isHttpGone()) {
+                log.infof("Received recoverable error for watch (message: %s)", cause.getMessage());
+
+                close();
+
+                log.info("Try to reconnect in 5 seconds");
+                executor.schedule(this::start, 5, TimeUnit.SECONDS);
+            } else {
+                // Note that this should not happen normally, since fabric8 client handles reconnect.
+                // In case it tries to reconnect this method is not called.
+                log.error("Unexpected error happened with watch. Will exit.", cause);
+            }
+        }
+
+        public void start() {
+            log.info("The watcher is starting...");
+
+            try {
+                namespace.ifPresentOrElse(
+                    ns -> {
+                        w = client.genericKubernetesResources(apiVersion, kind).inNamespace(ns).watch(
+                            timeout.isPresent()
+                                ? new ListOptionsBuilder().withTimeoutSeconds(timeout.get()).build()
+                                : new ListOptionsBuilder().build(),
+                            this);
+                    },
+                    () -> {
+                        w = client.genericKubernetesResources(apiVersion, kind).inAnyNamespace().watch(
+                            timeout.isPresent()
+                                ? new ListOptionsBuilder().withTimeoutSeconds(timeout.get()).build()
+                                : new ListOptionsBuilder().build(),
+                            this);
+                    });
+            } catch (KubernetesClientException e) {
+                log.infof("Received error while staring watcher, try to reconnect in 5 seconds  (message: %s)", e.getMessage());
+                executor.schedule(this::start, 5, TimeUnit.SECONDS);
+            }
+        }
+
+        @Override
+        public void close() {
+            try {
+                if (w != null) {
+                    w.close();
+                }
+            } catch (Exception e) {
+                log.error("Unexpected error happened when closing watcher.", e);
+            }
+        }
     }
 
-    private class ResourceHandler implements ResourceEventHandler<GenericKubernetesResource> {
+    private class ResourceHandler implements ResourceEventHandler<GenericKubernetesResource>, AutoCloseable {
+        private SharedIndexInformer<GenericKubernetesResource> i;
+
         @Override
         public void onAdd(GenericKubernetesResource resource) {
-            log.infof("Add, Namespace: %s, Name: %s", 
-                resource.getMetadata().getNamespace(), 
-                resource.getMetadata().getName()); 
-            
+            log.infof("Add, Namespace: %s, Name: %s",
+                resource.getMetadata().getNamespace(),
+                resource.getMetadata().getName());
+
         }
 
         @Override
         public void onUpdate(GenericKubernetesResource oldObj, GenericKubernetesResource resource) {
             log.infof("Update, Namespace: %s, Name: %s",
-                resource.getMetadata().getNamespace(), 
-                resource.getMetadata().getName()); 
-            
+                resource.getMetadata().getNamespace(),
+                resource.getMetadata().getName());
+
         }
 
         @Override
         public void onDelete(GenericKubernetesResource resource, boolean deletedFinalStateUnknown) {
             log.infof("Delete, Namespace: %s, Name: %s",
-                resource.getMetadata().getNamespace(), 
-                resource.getMetadata().getName()); 
-            
-        }  
+                resource.getMetadata().getNamespace(),
+                resource.getMetadata().getName());
+
+        }
+
+        public void start() {
+            log.info("The informer is starting...");
+
+            namespace.ifPresentOrElse(
+                ns -> {
+                    i = client.genericKubernetesResources(apiVersion, kind).inNamespace(ns).inform(this);
+                },
+                () -> {
+                    i = client.genericKubernetesResources(apiVersion, kind).inAnyNamespace().inform(this);
+                });
+        }
+
+        @Override
+        public void close() {
+            try {
+                if (i != null) {
+                    i.close();
+                }
+            } catch (Exception e) {
+                log.error("Unexpected error happened when closing informer.", e);
+            }
+        }
     }
 }
